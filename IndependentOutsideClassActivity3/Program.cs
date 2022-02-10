0d4b8f0890d8636dd5dbded7aa6801c2d5caf8be
student <student@DELL603953>
2022-02-10 12:57:19 +0800

    Add project files.

---
+++ IndependentOutsideClassActivity3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentOutsideClassActivity3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool intInput;
+            int numberInput;
+            Console.Write("Input a number: ");
+
+            intInput = int.TryParse(Console.ReadLine(), out numberInput);
+            switch (intInput)
+            {
+                case true:
+            switch (numberInput)
+            {
+                case 10: Console.WriteLine("Your number was 10");
+                    break;
+
+                default: Console.WriteLine("Your number was not 10");
+                    break;
+            }
+                    break;
+                case false: Console.WriteLine("You didn't input an integer");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
